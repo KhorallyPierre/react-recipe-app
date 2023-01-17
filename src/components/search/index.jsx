c1298c5fa8ec310fdf984917eed90284241b6636
Khorally <khorally.pierre@gmail.com>
2023-01-17 16:44:50 -0500

    passing data from one component to another

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -2,26 +2,33 @@ import { useState} from 'react';
 import './styles.css';
 
 // state management:
-//usestate
-// values get sotred as a state variable to be passed in 
+//usestate hook allows values to be changed whenever we want
+// values get stored as a state variable to be passed in 
 //userReducer is used to manage complex state (will be covered later)
 
 
 
 
-const Search = ()=> {
-// input value is for input, setINputValue is a function, to return updated state of input
-    const [inputValue, setInputValue] = useState('') //initial value of state, which is an empty string
+const Search = (props)=> {
+    const {getDataFromSearchComponent} = props;
+    console.log(props)
+// input value is initual value of input, setInputValue is a function 
+    const [inputValue, setInputValue] = useState('') //initial value of state, which is an empty string for a search bar
     
     const handleInputValue= (event) => {
         const {value} = event.target;
-        //set the updated state
+        //set the updated stateß
         setInputValue(value)
     }
     console.log(inputValue)
 
+    const handleSubmit = (event)=> {
+        event.preventDefault()
+        //this is where the data from search component get passed in to homepage component
+        getDataFromSearchComponent(inputValue)
+    }
     return (
-        <form className="Search">
+        <form onSubmit={handleSubmit} className="Search">
             <input name="search" onChange={handleInputValue} value={inputValue} placeholder="Search Recipes" id="search"/>
             <button type="submit"> Search </button>
         </form>
