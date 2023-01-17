6c08d56e60f2b8e55fe81b2a17b74c6d49be201e
Khorally <khorally.pierre@gmail.com>
2023-01-17 13:23:46 -0500

    homepage and search components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import Homepage from "./pages/homepage";
 // this is a functional component as it starts with the word function or includes an arrow function
 // component name should start with a capital letter
 // the component body is known as jsx (implements component logic
 // export that component to be used in other files 
 
 
-const App = () => {
-  return (
-    < div> This is our first component </div>
+function App() {
+  return ( <div className="App">
+      <Homepage/>
+  </div>
+    
   )
 }
 
