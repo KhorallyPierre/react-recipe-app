0dfaf2ff56d63ef137b500ce98aad46ba9b769e8
Khorally <khorally.pierre@gmail.com>
2023-01-16 19:06:30 -0500

    initializing app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// this is a functional component as it starts with the word function or includes an arrow function
+// component name should start with a capital letter
+// the component body is known as jsx (implements component logic
+// export that component to be used in other files 
+
+
+const App = () => {
+  return (
+    < div> This is our first component </div>
+  )
+}
+
+export default App
